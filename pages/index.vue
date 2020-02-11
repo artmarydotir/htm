28eb007df8f2a493eb88c493929f6379c9db0667
Art Mary <artmary.ir@gmail.com>
2020-02-11 22:47:29 +0330

    init htm.

---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-center">
+        <!-- <logo />
+        <vuetify-logo /> -->
+      </div>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to Htm
+        </v-card-title>
+        <v-card-text>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            sint vitae, omnis est consectetur, optio a magnam cupiditate odit,
+            minima nemo consequuntur deserunt voluptate. Dignissimos nesciunt
+            asperiores eveniet temporibus modi?
+          </p>
+          <div class="text-xs-right">
+            <em><small>&mdash; John Leider</small></em>
+          </div>
+          <hr class="my-3" />
+          <a href="https://nuxtjs.org/" target="_blank">
+            Nuxt Documentation
+          </a>
+          <br />
+          <a href="https://github.com/nuxt/nuxt.js" target="_blank">
+            Nuxt GitHub
+          </a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/inspire">
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+// import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    // Logo,
+    // VuetifyLogo
+  }
+}
+</script>
---
+++ vuetify.options.js
@@ -0,0 +1,25 @@
+import colors from 'vuetify/es5/util/colors'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+// vuetify.options.js
+export default function({ store }) {
+  return {
+    icons: {
+      iconfont: 'md'
+    },
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  }
+}
