8aafd2e710f52a06ccc2d03ab3b984bfe093e4fb
Art Mary <artmary.ir@gmail.com>
2020-02-16 23:23:39 +0330

    add certificate layout.

--- layouts/default.vue
+++ layouts/default.vue
@@ -111,7 +111,7 @@ export default {
           },
           {
             title: 'List Certificate',
-            link: '/service/list'
+            link: '/certificate/list'
           }
         ]
       }
---
+++ pages/certificate/list.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-content>
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12">
+          <p class="display-2 text-center justify-center pb-4">
+            list
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {};
+</script>
